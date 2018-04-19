183e443d005a37eae65dd4bc9c29c42df5a8863e
Geoffrey Harrison <Geoff.Harrison@bulletproof.net>
2018-04-19 12:59:33 +1000

    Update Messages

--- assets/update_build.sh
+++ assets/update_build.sh
@@ -10,22 +10,26 @@ LOCAL_VERSION=$(grep -Eo "[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}" assets/VERSION)
 # Check Versions And Perform Update Actions
 if [ "$SITE_VERSION" != "$LOCAL_VERSION" ]
 then
-  echo " Not Matched - Begining Update Process..."
-  git fetch --tags > /dev/null
+  echo " -- Not Matched - Begining Update Process..."
+  echo
+  git fetch --tags
   git tag | grep -q "${SITE_VERSION}"
   [ $? -eq 0 ] && echo "Version ${SITE_VERSION} Already Exists In Repository - No Action" && exit 1
-  echo " Version ${SITE_VERSION} Not Found. Updating Build..."
+  echo " -- Version ${SITE_VERSION} Not Found. Updating Build..."
+  echo
   git checkout master > /dev/null
   echo "${SITE_VERSION}" > assets/VERSION
   git add -A :/ > /dev/null
   git commit -m "Automated: Updating ${LOCAL_VERSION} To ${SITE_VERSION}" > /dev/null
   git tag -a "${SITE_VERSION}" -m "Firefox ESR and Tor Browser Version ${SITE_VERSION}" > /dev/null
-  echo " Pushing To Upstream..."
+  echo " -- Pushing To Upstream..."
+  echo
   git push origin master > /dev/null
   git push --tags origin > /dev/null
-  echo " Update Process Complete."
+  echo " -- Update Process Complete."
+  echo
 else
-  echo " Versions Match - No Action Required"
+  echo " -- Versions Match - No Action Required"
 fi
 
 exit 0
