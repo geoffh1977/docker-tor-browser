98b73e583591ec56b5eb936403a1f05985c38732
Geoffrey Harrison <geoffh1977@gmail.com>
2020-05-14 15:30:59 +1000

    Add Version Update Script

--- update_version.sh
+++ update_version.sh
@@ -8,14 +8,19 @@ VERSION=$(echo "${DOWNLOAD_FILE}" | grep -Eo "[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}
 REPO_TAGS=$(curl -s https://registry.hub.docker.com/v1/repositories/geoffh1977/tor-browser/tags  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')
 
 # Check if Current Version Is In Docker Hub
-if echo "${REPO_TAGS}" | grep -q "${VERSION}"
+RESULT=$(echo "${REPO_TAGS}" | grep "${VERSION}")
+if [ "x${RESULT}" == "x" ]
 then
   # Update Version - Triggers Docker Hub Build
+  echo
   echo "${VERSION}" > VERSION
+  git add VERSION
   git commit -m "Update To Version: ${VERSION}"
   git push origin master
+  echo
   echo "Updated Software To Version ${VERSION}"
 else
   # No New Updates
+  echo
   echo "Version ${VERSION} Already Exists In Docker Hub."
 fi
